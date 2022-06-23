3b62f47d16dab6bd51bb4e4d34de8ccd3c2ea7a6
Ganesh Naik <naikganeshg2n@gmail.com>
2022-06-24 00:04:12 +0530

    bloom code

--- AtomDB/src/main/java/Constants/DBConstants.java
+++
@@ -1,9 +0,0 @@
-package Constants;
-
-import com.google.common.primitives.Longs;
-
-public class DBConstants {
-    public static final int MB = 1_000_000;
-    public static final byte[] EOF = new byte[] {-1, -1, -1, -1, -1, -1, -1, -1};
-    public final static int LongLength = Longs.toByteArray(1L).length;
-}
--- AtomDB/src/main/java/SsTable/FileOperations.java
+++
@@ -1,5 +0,0 @@
-package SsTable;
-
-import java.util.Scanner;
-public class FileOperations {
-}
