f44201923d2c969d531f294c48815637c88f081e
GaneshNaik <naikganeshg2n@gmail.comm>
2025-07-05 01:00:10 +0530

    unit test for memTable

--- AtomDB/src/main/java/org/g2n/atomdb/Mem/SkipListMemtable.java
+++ AtomDB/src/main/java/org/g2n/atomdb/Mem/SkipListMemtable.java
@@ -1,9 +1,11 @@
 package org.g2n.atomdb.Mem;
 
+import com.google.common.base.Preconditions;
 import org.g2n.atomdb.db.KVUnit;
 
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.SortedMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
@@ -13,6 +15,8 @@ public class SkipListMemtable implements MutableMem<byte[], KVUnit>{
     private int currentSize;
 
     public SkipListMemtable(int maxSize, Comparator<byte[]> comparator) {
+        Preconditions.checkArgument(maxSize > 0, "Max size must be greater than 0");
+
         this.maxSize = maxSize;
         map = new ConcurrentSkipListMap<>(comparator);
         currentSize = 0;
@@ -20,17 +24,26 @@ public class SkipListMemtable implements MutableMem<byte[], KVUnit>{
 
     @Override
     public void put(KVUnit kvUnit) {
-        map.put(kvUnit.getKey(), kvUnit);
+        Objects.requireNonNull(kvUnit, "KVUnit cannot be null");
+        KVUnit alreadyExisting = map.put(kvUnit.getKey(), kvUnit);
+        if (alreadyExisting != null) {
+            currentSize -= alreadyExisting.getUnitSize();
+        }
         currentSize += kvUnit.getUnitSize();
     }
 
     @Override
     public void delete(KVUnit kvUnit) {
+        Objects.requireNonNull(kvUnit, "KVUnit cannot be null");
+        if (!kvUnit.isDeleted()) {
+            throw new IllegalArgumentException("Cannot delete a non-deleted KVUnit");
+        }
         put(kvUnit);
     }
 
     @Override
     public KVUnit get(byte[] key) {
+        Objects.requireNonNull(key, "key cannot be null");
         return map.get(key);
     }
 
